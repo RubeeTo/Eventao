6b18f1f8ff735cb2b805d74588ad7d31397d0260
rubeeTo <estudo.rub@gmail.com>
2024-05-27 14:40:40 -0300

    atualização do Worksspace

--- app/src/main/java/com/example/app3/RegisterActivity.kt
+++ app/src/main/java/com/example/app3/RegisterActivity.kt
@@ -1,12 +1,15 @@
 package com.example.app3
 
+import android.R
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+
 
 class RegisterActivity : AppCompatActivity() {
 
