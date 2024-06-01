04f9076f23693ae9a435cb9796094eead23ce9f0
rubeeTo <estudo.rub@gmail.com>
2024-05-31 23:53:38 -0300

    Layout de tela sobre detalhes do Evento do cliente e admin. Layout o item dinamico para listagem de clientes no evento

---
+++ app/src/main/java/com/example/app3/activity_event_detail_admin.kt
@@ -0,0 +1,14 @@
+package com.example.app3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class activity_event_detail_admin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_detail_admin)
+    }
+}
